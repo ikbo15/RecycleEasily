ea3391d9eb7c2e7a369ab6088452babf8e6fc3bc
vignatov62 <vignatov62@yandex.ru>
2016-06-28 20:33:08 +0300

    Карта фандоматов
    Добавлены комментарии к коду

---
+++ Fandomate_map/map.js
@@ -0,0 +1,43 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+        center: [55.751574, 37.573856],
+        zoom: 11,
+        behaviors: ['default', 'scrollZoom'],
+        controls: ['geolocationControl']  //Кнопка геолокации
+    }, {
+        searchControlProvider: 'yandex#search'
+}),
+
+clusterer = new ymaps.Clusterer({    //Создание кластера
+    preset: 'islands#invertedVioletClusterIcons',
+    groupByCoordinates: false,
+    clusterDisableClickZoom: true,
+    clusterHideIconOnBalloonOpen: false,
+    geoObjectHideIconOnBalloonOpen: false
+}),
+
+getPointData = function (index) { //изменить параметры
+    return {
+        balloonContentHeader: points[i].name,   //Заголовок балуна, передается название объекта
+        balloonContentBody: "<b>Адрес: </b>" + points[i].address,  //Вывод адреса в основной блок балуна
+        hintContent: points[i].name,  //Подсказка при наведении на метку, вывод названия объекта
+    };
+},
+
+getPointOptions = function () {
+    return {
+        preset: 'islands#violetIcon'  //Выбор отображения метки
+    };
+},
+
+geoObjects = [];
+
+for(var i = 0, len = points.length; i < len; i++) { //Цикл для вывода кажоый метки
+    var myPlacemark = new ymaps.Placemark(points[i].coordinates, getPointData(i), getPointOptions());
+    geoObjects[i] = myPlacemark;
+}
+
+clusterer.add(geoObjects);
+myMap.geoObjects.add(clusterer);
+
+});
