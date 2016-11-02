64f9b39ed3a843b74d26205f73429c44dd59443e
vignatov62 <vignatov62@yandex.ru>
2016-11-02 17:18:11 +0300

    Описание классов
    Описание классов базы данных, API и карты

---
+++ database.cpp
@@ -0,0 +1,33 @@
+#include "database.h"
+
+bool Database::search(string s)
+{
+
+}
+
+bool Database::execQuery(string s)
+{
+
+}
+
+bool Database::save(string s)
+{
+
+}
+
+bool Database::sendAc(string password, string email)
+{
+
+}
+
+bool Database::findUser(string s)
+{
+
+}
+
+bool Database::addUser(string s)
+{
+
+}
+
+//getPoints
